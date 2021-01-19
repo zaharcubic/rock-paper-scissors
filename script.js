e48b5ff05d9673fdefc0934ca4b54dfca5c520a8
zaharcubic <victoriaseptimiu@gmail.com>
2021-01-19 15:42:07 +0200

    Completed the minigame adding all of the functions

--- script.js
+++ script.js
@@ -2,5 +2,38 @@
 function computerPlay() {
     const availablePicks = ["rock", "paper", "scissors"];
     const comPick = availablePicks[Math.floor(Math.random() * availablePicks.length)];
-    console.log(comPick);
+    console.log("Computer picks " + comPick)
+    return comPick;
 }
+//create a function that plays a single round
+//must have two paramenters - playerSelection and computerSelection
+//must return a string that declares the winner
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "Computer wins";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "Player wins"
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "Player Wins";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "Computer Wins"
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "Computer Wins"
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "Player Wins"
+    }
+}
+//function in which the game plays 5 rounds automatically
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const computerSelection = computerPlay();
+        playRound();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+//gets input from the player and the converts it into lowercase to make it case insensitive
+let playerSelection = prompt("Please pick from rock, paper, or scissors");
+playerSelection = playerSelection.toLowerCase();
