c1cdff7e463245db208c5dafcb61459ebfc49e3c
zaharcubic <victoriaseptimiu@gmail.com>
2021-01-28 15:39:26 +0200

    Added a styling file and all of the functionalities of the game

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@ let playerSelection;
 let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
-
+const declaration = document.querySelector('#declaration');
 
 function computerPlay() {
     const availablePicks = ["rock", "paper", "scissors"];
@@ -16,22 +16,32 @@ function playRound(playerSelection, computerSelection) {
     // playerSelection = prompt("Please pick from rock, paper, or scissors");
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection) {
-        console.log("It's a tie");
+        declaration.textContent = 'It\'s a tie!'
     } else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')) {
         playerScore = ++playerScore;
-        console.log(`${computerScore} - ${playerScore}`);
+        declaration.textContent = `Player wins this round! Computer chose ${computerSelection}`;
     } else {
         computerScore = ++computerScore;
-        console.log(`${computerScore} - ${playerScore}`);
+        declaration.textContent = `Computer Wins! Computer chose ${computerSelection}`;
     };
-    
+    const score = document.querySelector('#score');
+    score.textContent = `The score is: Player: ${playerScore} - Computer: ${computerScore}`;
 };
+
+
+
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
     playerSelection = 'rock';
     playRound(playerSelection, computerSelection);
-    if(playerScore == 5 || computerScore == 5){
-        console.log('Winner Declaration');
+    if (playerScore == 5) {
+        alert('Player Wins!');
+        computerScore = 0;
+        playerScore = 0;
+    } else if (computerScore == 5){
+        alert('Computer Wins!');
+        computerScore = 0;
+        playerScore = 0;
     }
 });
 
@@ -39,8 +49,14 @@ const paper = document.querySelector('#paper');
 paper.addEventListener('click', () => {
     playerSelection = 'paper';
     playRound(playerSelection, computerSelection);
-    if(playerScore == 5 || computerScore == 5){
-        console.log('Winner Declaration');
+    if (playerScore == 5) {
+        alert('Player Wins!');
+        computerScore = 0;
+        playerScore = 0;
+    } else if (computerScore == 5){
+        alert('Computer Wins!');
+        computerScore = 0;
+        playerScore = 0;
     }
 });
 
@@ -48,8 +64,14 @@ const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', () => {
     playerSelection = 'scissors';
     playRound(playerSelection, computerSelection);
-    if(playerScore == 5 || computerScore == 5){
-        console.log('Winner Declaration');
+    if (playerScore == 5) {
+        alert('Player Wins!');
+        computerScore = 0;
+        playerScore = 0;
+    } else if (computerScore == 5){
+        alert('Computer Wins!');
+        computerScore = 0;
+        playerScore = 0;
     }
 });
 
